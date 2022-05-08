d80cbef9814720d42a6283a48fa01fd476a7669d
shkna1368 <sh.k.na.1368@gmail.com>
2022-05-08 21:55:01 +0430

    add first app of session 1

---
+++ session1/FirstApplication/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+
+
+    public static void main(String[] args) {
+
+        System.out.println(" HI java ") ;
+        System.out.println("سلاو بو کوردوستان");
+        System.out.println("سلاو بو کوردوستان");
+
+    }
+
+}
