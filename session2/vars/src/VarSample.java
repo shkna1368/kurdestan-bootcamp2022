54c83ffeaec9e4abb488abd3af09f680828d23b5
shkna1368 <sh.k.na.1368@gmail.com>
2022-05-10 21:49:26 +0430

    add seesion 2

---
+++ session2/vars/src/VarSample.java
@@ -0,0 +1,31 @@
+public class VarSample {
+    public static void main(String[] args) {
+
+       int myIntVar=1000000;
+       long myLongValue=2000003266;
+       double mDoubleValue=2.3225;
+       float myFloatValue=6.1f;
+       char myCharValue='a';
+       String myStringValue="I Love Kurdestan";
+
+
+       var x=10;
+       var myString="12Abc";
+
+
+        System.out.println(myIntVar);
+        System.out.println(x);
+        System.out.println(myLongValue);
+        System.out.println(myCharValue);
+        System.out.println(myStringValue);
+        System.out.println("---------------------");
+        int x1=25;
+        System.out.println(x1);
+        x1=63;
+        System.out.println(x1);
+
+
+
+    }
+
+}
