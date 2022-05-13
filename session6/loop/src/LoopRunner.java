9d165c20771aafe42144260f546f3a5e788ccf64
shkna1368 <sh.k.na.1368@gmail.com>
2022-05-13 16:41:43 +0430

    add seesion 6

---
+++ session6/loop/src/LoopRunner.java
@@ -0,0 +1,19 @@
+public class LoopRunner {
+
+    public static void main(String[] args) {
+
+
+        for(int i=0; i<1000;i++) {
+            System.out.println("index "+i +"=Kurdestan");
+        }
+
+
+        int j=0;
+        while (j<10){
+
+            System.out.println(j+"Kurdestan");
+           j++;
+        }
+
+    }
+}
