48e21ccd80aad97e5e9ad8dce3df22a5c154e422
shkna1368 <sh.k.na.1368@gmail.com>
2023-03-07 12:57:25 +0330

    add session 127 pbservibility

---
+++ session127/observability-boot-blog-post-main/server/src/main/java/com/example/server/ServerApplication.java
@@ -0,0 +1,123 @@
+package com.example.server;
+
+import java.util.Collections;
+import java.util.Random;
+import java.util.stream.StreamSupport;
+
+import io.micrometer.common.KeyValue;
+import io.micrometer.observation.Observation;
+import io.micrometer.observation.ObservationHandler;
+import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.observation.annotation.Observed;
+import io.micrometer.observation.aop.ObservedAspect;
+import jakarta.servlet.DispatcherType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+	// tag::aspect[]
+	// To have the @Observed support we need to register this aspect
+	@Bean
+	ObservedAspect observedAspect(ObservationRegistry observationRegistry) {
+		return new ObservedAspect(observationRegistry);
+	}
+	// end::aspect[]
+
+}
+
+// tag::controller[]
+@RestController
+class MyController {
+
+	private static final Logger log = LoggerFactory.getLogger(MyController.class);
+	private final MyUserService myUserService;
+
+	MyController(MyUserService myUserService) {
+		this.myUserService = myUserService;
+	}
+
+	@GetMapping("/user/{userId}")
+	String userName(@PathVariable("userId") String userId) {
+		log.info("Got a request");
+		return myUserService.userName(userId);
+	}
+}
+// end::controller[]
+
+// tag::service[]
+@Service
+class MyUserService {
+
+	private static final Logger log = LoggerFactory.getLogger(MyUserService.class);
+
+	private final Random random = new Random();
+
+	// Example of using an annotation to observe methods
+	// <user.name> will be used as a metric name
+	// <getting-user-name> will be used as a span  name
+	// <userType=userType2> will be set as a tag for both metric & span
+	@Observed(name = "user.name",
+			contextualName = "getting-user-name",
+			lowCardinalityKeyValues = {"userType", "userType2"})
+	String userName(String userId) {
+		log.info("Getting user name for user with id <{}>", userId);
+		try {
+			Thread.sleep(random.nextLong(200L)); // simulates latency
+		}
+		catch (InterruptedException e) {
+			throw new RuntimeException(e);
+		}
+		return "foo";
+	}
+}
+// end::service[]
+
+// tag::handler[]
+// Example of plugging in a custom handler that in this case will print a statement before and after all observations take place
+@Component
+class MyHandler implements ObservationHandler<Observation.Context> {
+
+	private static final Logger log = LoggerFactory.getLogger(MyHandler.class);
+
+	@Override
+	public void onStart(Observation.Context context) {
+		log.info("Before running the observation for context [{}], userType [{}]", context.getName(), getUserTypeFromContext(context));
+	}
+
+	@Override
+	public void onStop(Observation.Context context) {
+		log.info("After running the observation for context [{}], userType [{}]", context.getName(), getUserTypeFromContext(context));
+	}
+
+	@Override
+	public boolean supportsContext(Observation.Context context) {
+		return true;
+	}
+
+	private String getUserTypeFromContext(Observation.Context context) {
+		return StreamSupport.stream(context.getLowCardinalityKeyValues().spliterator(), false)
+				.filter(keyValue -> "userType".equals(keyValue.getKey()))
+				.map(KeyValue::getValue)
+				.findFirst()
+				.orElse("UNKNOWN");
+	}
+}
+// end::handler[]
